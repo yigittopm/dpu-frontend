58dc34eef4a762704a4f6768108dad502c84bf41
yigittopm <yigittopm@hotmail.com>
2022-02-23 14:06:47 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [hello, setHello] = useState("hello");
+
+  const data = async () => {
+    const text = await axios.get("https://dpu-backend.herokuapp.com/");
+    const a = await text.data;
+    setHello(a);
+  };
+
+  useEffect(() => {
+    data();
+  }, []);
+
   return (
     <div className="App">
       <h2>Mert Yigittop</h2>
+      <h4>{hello}</h4>
     </div>
   );
 }
