4c3ce5e97b1efe6625be8f185fea30a6f2824b77
yigittopm <yigittopm@hotmail.com>
2022-02-26 22:13:43 +0300

    path and href fix

--- src/compotents/TopBar.js
+++ src/compotents/TopBar.js
@@ -21,17 +21,17 @@ function TopBar() {
           </button>
           <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
             <li>
-              <a class="dropdown-item" href="#">
+              <a class="dropdown-item" href="/">
                 Action
               </a>
             </li>
             <li>
-              <a class="dropdown-item" href="#">
+              <a class="dropdown-item" href="/">
                 Another action
               </a>
             </li>
             <li>
-              <a class="dropdown-item" href="#">
+              <a class="dropdown-item" href="/">
                 Something else here
               </a>
             </li>
