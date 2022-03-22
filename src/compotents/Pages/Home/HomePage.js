7b7084c66625f22a8844c3d35fada3bd5cafe189
yigittopm <yigittopm@hotmail.com>
2022-03-22 19:31:41 +0300

    deploy unused items deleted

--- src/compotents/Pages/Home/HomePage.js
+++ src/compotents/Pages/Home/HomePage.js
@@ -1,13 +1,13 @@
 //import axios from "axios";
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getAllProducts } from "../../../redux/product/productSlice";
+import React from "react";
+//import { useDispatch } from "react-redux";
+//import { getAllProducts } from "../../../redux/product/productSlice";
 //import { Rings } from "react-loader-spinner";
 import LoadingPage from "../LoadingPage";
 import Products from "./Products";
 
 function HomePage() {
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
   //const productSlice = useSelector((state) => state.product);
   //const [products, setProducts] = useState([]);
   //const [loading, setLoading] = useState(true);
