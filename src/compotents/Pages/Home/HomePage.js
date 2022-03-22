5291b4c31609638270e21c627a0373ec3bf75811
yigittopm <yigittopm@hotmail.com>
2022-03-22 19:29:50 +0300

    deploy test

--- src/compotents/Pages/Home/HomePage.js
+++ src/compotents/Pages/Home/HomePage.js
@@ -1,20 +1,21 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+//import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { getAllProducts } from "../../../redux/product/productSlice";
-import { Rings } from "react-loader-spinner";
+//import { Rings } from "react-loader-spinner";
 import LoadingPage from "../LoadingPage";
 import Products from "./Products";
 
 function HomePage() {
   const dispatch = useDispatch();
-  const productSlice = useSelector((state) => state.product);
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  //const productSlice = useSelector((state) => state.product);
+  //const [products, setProducts] = useState([]);
+  //const [loading, setLoading] = useState(true);
+  const loading = true;
 
-  useEffect(() => {
-    dispatch(getAllProducts());
-  }, []);
+  // useEffect(() => {
+  //   dispatch(getAllProducts());
+  // }, []);
 
   return <div className="">{loading ? <Products /> : <LoadingPage />}</div>;
 }
